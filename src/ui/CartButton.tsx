a2407405037e9de1c918d879d2117b1296573df6
danilkolesnik <apparentlysolved@gmail.com>
2025-03-23 22:39:58 +0200

    ui: cart dropdown

--- src/ui/CartButton.tsx
+++ src/ui/CartButton.tsx
@@ -1,10 +1,19 @@
-import { useState } from 'react';
+'use client';
+import { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import styles from './cart.button.module.scss';
+import { localStorageService } from '@/services/localStorage';
 import { CartIcon } from '@/assets/icons/icons';
 import CartDropdown from './CartDropdown';
 
 export default function CartButton() {
+
+  const [cart, setCart] = useState([]);
+  
+  useEffect(() => {
+      setCart(localStorageService({method: 'get', key: 'cart'}));
+  }, []);
+
   const [isDropdownVisible, setDropdownVisible] = useState(false);
 
   return (
@@ -13,7 +22,7 @@ export default function CartButton() {
       onMouseEnter={() => setDropdownVisible(true)}
       onMouseLeave={() => setDropdownVisible(false)}
     >
-      <button className={styles.cartButton} type="button">
+      <button className={`${styles.cartButton} ${cart?.length > 0 && styles.active}`} type="button">
         <span className={styles.invisibleButtonSpan}></span>
         <span className={styles.buttonContent}>
           <span className={styles.cartButtonCaption}>Твiй кошик</span>
@@ -27,7 +36,7 @@ export default function CartButton() {
         transition={{ duration: 0.2, ease: 'easeOut' }}
         style={{ display: isDropdownVisible ? 'block' : 'none' }}
       >
-        <CartDropdown />
+        {cart?.length > 0 && <CartDropdown />}
       </motion.div>
     </div>
   );
--- src/ui/CartDropdown.tsx
+++ src/ui/CartDropdown.tsx
@@ -1,7 +1,46 @@
+'use client';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 import styles from './cart.dropdown.module.scss';
+import { localStorageService } from '@/services/localStorage';
+import { TrashIcon } from '@/assets/icons/icons';
 
 export default function CartDropdown() {
+    const router = useRouter();
+    const [cart, setCart] = useState([]);
+
+    useEffect(() => {
+        setCart(localStorageService({method: 'get', key: 'cart'}));
+        // console.log(localStorageService({method: 'get', key: 'cart'}));
+    }, []);
+
     return (
-        <div className={styles.cartDropdown}>klusha</div>
+        <div className={styles.cartDropdown}>
+            <ul className={styles.cartItemsList}>
+                {(cart || []).map((item: any) => (
+                    <li key={item.id} onClick={() => router.push(`/${item.handle}`)} className={styles.miniCartItem}>
+                        <div className={styles.productPhotoWrapper}>
+                            {item.images && <img src={item.images[0].url} alt="" />}
+                        </div>
+                        <div className={styles.productDetailsWrapper}>
+                            <div className={styles.productInfoFlex}>
+                                <span className={styles.productName}>
+                                    {item.title}
+                                </span>
+                                <span className={styles.productPrice}>
+                                    21 999 грн
+                                </span>
+                            </div>
+                        </div>
+                        <div className={styles.productRemoveWrapper}>
+                            <TrashIcon />
+                        </div>
+                    </li>
+                ))}
+            </ul>
+            <div className={styles.goToCartWrapper}>
+                <button onClick={() => router.push('/cart')} className={styles.goToCartButton}>Оформити замовлення</button>
+            </div>
+        </div>
     );
 };
